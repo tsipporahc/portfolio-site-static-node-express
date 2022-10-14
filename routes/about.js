fb58e66e92a51d89f4146f32b065292cd147d518
tsipporahc <tsipporahc@gmail.com>
2022-10-14 16:39:24 -0700

    Updates styles for sidebar

--- routes/about.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router(); // create route handlers
-
-// define the about route
-router.get('/', (req, res) => {
-    res.render('about');
-});
-
-module.exports = router; //export router to app.js
